3b550c87ae866ff21573b139f881f53c15297a1d
mcoker <mcoker@purdue.edu>
2017-08-28 22:08:20 -0400

    Repo and project initialization.

---
+++ tutorReserve/tutorReserve/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace tutorReserve.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
