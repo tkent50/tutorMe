1cee15cd1580489dd4336a48e52311e3f7489e68
Srish02 <gupta290@purdue.edu>
2017-10-25 21:58:41 -0400

    TutorSettings Page Add Class

---
+++ tutorMe/WebApplication5/Scripts/tutor_setting.js
@@ -0,0 +1,41 @@
+﻿function onTutorSettingPageLoad() {
+    alert();
+    tutorSettingLoadClasses();
+}
+
+
+function tutorSettingLoadClasses() {
+    //alert("loading");
+    $.ajax({
+        url: "TutorSearch.aspx/GetClasses",
+        method: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("Request: " + XMLHttpRequest.toString() + "\n\nStatus: " + textStatus + "\n\nError: " + errorThrown);
+        },
+        success: function (result) {
+            //alert("success");
+            console.log(result);
+            tutorSettingHandleClasses(result);
+        }
+    });
+}
+
+function tutorSettingHandleClasses(classList) {
+    var numClasses = parseInt(classList.d[0]);
+    for (var i = 1; i <= numClasses; i++) {
+
+        var newTab = document.createElement("LI");
+        newTab.value = classList.d[i];
+        newTab.onclick = function (event) {
+            document.getElementById("search").placeholder = this.innerHTML;
+        }
+        newTab.appendChild(document.createTextNode(classList.d[i]));
+
+        // newTab.appendChild(newClassName);
+        //newTab.appendChild(testList);
+        console.log(classList);
+        document.getElementById("classList").appendChild(newTab);
+    }
+}
